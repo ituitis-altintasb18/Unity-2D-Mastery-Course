d903a8fd84cfe86463bc1b48689ec1272e019374
ituitis-altintasb18 <56095106+ituitis-altintasb18@users.noreply.github.com>
2022-12-22 16:08:22 +0300

    Method and Coinbox
    Easier Methods and Coinbox adjustments

--- Assets/Scripts/Coinbox.cs
+++ Assets/Scripts/Coinbox.cs
@@ -19,17 +19,30 @@ public class Coinbox : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(remainingCoins > 0 &&
-            collision.collider.GetComponent<PlayerMovementController>() != null)
+        if (remainingCoins > 0 &&                                       //checks if there is still coin in the Coinbox
+            WasHitByPlayer(collision) &&          
+            WasHitFromBottomSide(collision))                                           
         {
-            Debug.Log(collision.contacts[0].normal);
+
             GameManager.Instance.AddCoin();
             remainingCoins--;
-            if(remainingCoins <= 0)
+            if (remainingCoins <= 0)
             {
                 enabledSprite.enabled = false;
                 disabledSprite.enabled = true;
             }
+
+
         }
+    }
+
+    private static bool WasHitByPlayer(Collision2D collision)           //checks if the contact is a player
+    {
+        return collision.collider.GetComponent<PlayerMovementController>() != null;
+    }
+
+    private static bool WasHitFromBottomSide(Collision2D collision)     //checking the contact location of the CoinBox
+    {
+        return collision.contacts[0].normal.y > 0.5;
     }
 }
