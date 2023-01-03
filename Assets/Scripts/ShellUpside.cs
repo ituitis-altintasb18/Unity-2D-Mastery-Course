92d8b200480a5804f3ed5d6cfba9503cb4ded217
ituitis-altintasb18 <56095106+ituitis-altintasb18@users.noreply.github.com>
2023-01-03 11:02:35 +0300

    The shell is like surfin board now xd
    The shell now launches and goes brr and if it hits you back it kills you

---
+++ Assets/Scripts/ShellUpside.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellUpside : MonoBehaviour
+{
+    [SerializeField]
+    private float shellSpeed = 5f;
+
+    private new Collider2D collider;
+    private new Rigidbody2D rigidbody2D;
+    private Vector2 direction;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(direction.x * shellSpeed, rigidbody2D.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.WasHitByPlayer())
+        {
+            if (direction.magnitude == 0)
+            {
+                LaunchShell(collision);
+            }
+            else
+            {
+                if (collision.WasHitFromUpperSide())
+                {
+                    direction = Vector2.zero;
+                }
+                else
+                {
+                    GameManager.Instance.KillPlayer();
+                }
+            }
+        }
+    }
+
+    private void LaunchShell(Collision2D collision)
+    {
+        float floatDirection = collision.contacts[0].normal.x > 0 ? 1f : -1f;
+        direction = new Vector2(floatDirection, 0);
+    }
+}
